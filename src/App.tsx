26ae40c985ead4862a64202ad026aaf4c35dd8ba
ganesh <ganesh.gawali@thinkitive.com>
2025-06-20 03:39:54 +0530

    Fix Vite configuration and project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a basic React application deployed on Netlify.</p>
+    </div>
+  )
+}
+
+export default App 
